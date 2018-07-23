2291d56f22c8d77a1e6ef5fbf8bcf7db19615167
Marcin Kossakowski <marcin.kossakowski@gmail.com>
2018-07-22 21:12:39 -0400

    added cache functionality for synthesized text.

--- api/src/main/scala/api/Speak.scala
+++ api/src/main/scala/api/Speak.scala
@@ -1,44 +1,58 @@
 package api
 
-import java.io.InputStream
+import java.io.{ByteArrayOutputStream, InputStream, OutputStream}
 
 import com.amazonaws.ClientConfiguration
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.services.polly.{AmazonPollyClient, AmazonPollyClientBuilder}
 import com.amazonaws.services.polly.model._
-import java.io.ByteArrayOutputStream
-
 import com.typesafe.scalalogging.Logger
 import core.WorkoutUseCase
 
+import scala.collection.mutable
+
 class Speak {
-  val logger = Logger(classOf[WorkoutUseCase])
+  private val logger = Logger(classOf[WorkoutUseCase])
+  private val cache = mutable.Map[Int, ByteArrayOutputStream]()
 
   private val client = AmazonPollyClientBuilder.standard()
     .withRegion("us-east-1")
     .withCredentials(new ProfileCredentialsProvider())
     .build()
 
-  private val reqest = new SynthesizeSpeechRequest()
-    .withOutputFormat(OutputFormat.Mp3)
-    .withVoiceId("Joanna")
-
-
+  /**
+    * Synthesize text to mp3 bytes.
+    * Store synthesized data in a Map and attempt to lookup same message instead of calling external API.
+    * @param msg
+    * @return
+    */
   def talk(msg: String): ByteArrayOutputStream = {
-    logger.info(s"Synthesizing text $msg")
+    if (cache.contains(msg.hashCode)) {
+      logger.info(s"Reading previously synthesized text: $msg")
 
-    val resp: SynthesizeSpeechResult = client.synthesizeSpeech(reqest.withText(msg))
-    val is: InputStream = resp.getAudioStream
+      cache.getOrElse(msg.hashCode, new ByteArrayOutputStream())
+    } else {
+      logger.info(s"Synthesizing text: $msg")
 
-    val bos = new ByteArrayOutputStream
+      val request = new SynthesizeSpeechRequest()
+        .withOutputFormat(OutputFormat.Mp3)
+        .withVoiceId("Joanna")
 
-    val bytes = new Array[Byte](1024)
-    var len = 0
+      val resp: SynthesizeSpeechResult = client.synthesizeSpeech(request.withText(msg))
+      val is: InputStream = resp.getAudioStream
 
-    while ( {len = is.read(bytes); len } > 0)
-      bos.write(bytes, 0, len)
-    is.close()
+      val bos = new ByteArrayOutputStream
 
-    bos
+      val bytes = new Array[Byte](1024)
+      var len = 0
+
+      while ( {len = is.read(bytes); len } > 0)
+        bos.write(bytes, 0, len)
+      is.close()
+
+      // Add output stream to the cache
+      cache += (msg.hashCode -> bos)
+      bos
+    }
   }
 }
